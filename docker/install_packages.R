e6486a5a9599ad1db4e294d225bd4ca774f9d0d8
sufyazi <sufyazi@outlook.com>
2024-03-11 16:38:15 +0800

    add Arrow R package to the container image

--- docker/install_packages.R
+++ docker/install_packages.R
@@ -1,4 +1,6 @@
 install.packages("readr")
+install.packages("arrow")
+install.packages("dplyr")
 # install BiocManager from CRAN (if not already installed)
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
---
+++ test-script/test.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript --vanilla
+
+.libPaths()
+library("arrow")
+# print hello world
+print("hello world!")
